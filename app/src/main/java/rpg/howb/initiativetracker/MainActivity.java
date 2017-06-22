0f748df42f2c6c22de875b9df4fa832ee2352855
Blake-Howard <blakeaaronhoward@gmail.com>
2017-06-22 16:48:30 -0500

    Display Basic List

--- app/src/main/java/rpg/howb/initiativetracker/MainActivity.java
+++ app/src/main/java/rpg/howb/initiativetracker/MainActivity.java
@@ -2,13 +2,23 @@ package rpg.howb.initiativetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Basic Array of strings
+    String[] players = {"Elzaron", "Dalben", "Tordic", "Avacat", "Magnolia"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Repo Test
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, players);
+
+        ListView listView = (ListView) findViewById(R.id.player_list);
+        listView.setAdapter(adapter);
     }
 }
