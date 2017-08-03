42a3620d48d695d2b918a3dead2c7714f87809af
Blake-Howard <blakeaaronhoward@gmail.com>
2017-08-03 13:41:01 -0500

    Add Player Class. Add conditions and getter setter methods

---
+++ app/src/main/java/rpg/howb/initiativetracker/Player.java
@@ -0,0 +1,201 @@
+package rpg.howb.initiativetracker;
+
+/**
+ * Created by howb on 8/3/2017.
+ */
+
+public class Player {
+
+    //Basic player information
+    private String name;
+    private int hp;
+    private int ac;
+    private int initiative;
+
+    //constructor
+    public Player(String name){
+        name = this.name;
+    }
+
+    //condition variables
+    private int exhaustion;
+    private boolean blinded;
+    private boolean charmed;
+    private boolean deafened;
+    private boolean exhausted;
+    private boolean frightened;
+    private boolean grappled;
+    private boolean incapacitated;
+    private boolean invisible;
+    private boolean paralyzed;
+    private boolean petrified;
+    private boolean poisoned;
+    private boolean prone;
+    private boolean restrained;
+    private boolean stunned;
+    private boolean unconscious;
+
+//**********************
+    //Condition getter and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAc() {
+        return ac;
+    }
+
+    public void setAc(int ac) {
+        this.ac = ac;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+
+    public int getExhaustion() {
+        return exhaustion;
+    }
+
+    public void setExhaustion(int exhaustion) {
+        this.exhaustion = exhaustion;
+    }
+
+    public boolean isBlinded() {
+        return blinded;
+    }
+
+    public void setBlinded(boolean blinded) {
+        this.blinded = blinded;
+    }
+
+    public boolean isCharmed() {
+        return charmed;
+    }
+
+    public void setCharmed(boolean charmed) {
+        this.charmed = charmed;
+    }
+
+    public boolean isDeafened() {
+        return deafened;
+    }
+
+    public void setDeafened(boolean deafened) {
+        this.deafened = deafened;
+    }
+
+    public boolean isExhausted() {
+        return exhausted;
+    }
+
+    public void setExhausted(boolean exhausted) {
+        this.exhausted = exhausted;
+    }
+
+    public boolean isFrightened() {
+        return frightened;
+    }
+
+    public void setFrightened(boolean frightened) {
+        this.frightened = frightened;
+    }
+
+    public boolean isGrappled() {
+        return grappled;
+    }
+
+    public void setGrappled(boolean grappled) {
+        this.grappled = grappled;
+    }
+
+    public boolean isIncapacitated() {
+        return incapacitated;
+    }
+
+    public void setIncapacitated(boolean incapacitated) {
+        this.incapacitated = incapacitated;
+    }
+
+    public boolean isInvisible() {
+        return invisible;
+    }
+
+    public void setInvisible(boolean invisible) {
+        this.invisible = invisible;
+    }
+
+    public boolean isParalyzed() {
+        return paralyzed;
+    }
+
+    public void setParalyzed(boolean paralyzed) {
+        this.paralyzed = paralyzed;
+    }
+
+    public boolean isPetrified() {
+        return petrified;
+    }
+
+    public void setPetrified(boolean petrified) {
+        this.petrified = petrified;
+    }
+
+    public boolean isPoisoned() {
+        return poisoned;
+    }
+
+    public void setPoisoned(boolean poisoned) {
+        this.poisoned = poisoned;
+    }
+
+    public boolean isProne() {
+        return prone;
+    }
+
+    public void setProne(boolean prone) {
+        this.prone = prone;
+    }
+
+    public boolean isRestrained() {
+        return restrained;
+    }
+
+    public void setRestrained(boolean restrained) {
+        this.restrained = restrained;
+    }
+
+    public boolean isStunned() {
+        return stunned;
+    }
+
+    public void setStunned(boolean stunned) {
+        this.stunned = stunned;
+    }
+
+    public boolean isUnconscious() {
+        return unconscious;
+    }
+
+    public void setUnconscious(boolean unconscious) {
+        this.unconscious = unconscious;
+    }
+    //*****************************
+    //End condition getter and setter methods
+}
